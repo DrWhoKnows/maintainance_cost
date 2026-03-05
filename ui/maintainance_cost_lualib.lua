d8a2f45a4d8258106884b44a3e70828e9a3b4d02
DrWhoKnows <yg421@qq.com>
2026-03-05 23:14:32 +0100

    ship info ui done

--- ui/maintainance_cost_lualib.lua
+++ ui/maintainance_cost_lualib.lua
@@ -1,12 +1,9 @@
 local ffi = require ("ffi")
 local C = ffi.C
-ffi.cdef[[
-  uint64_t ConvertStringTo64Bit(const char* idstring);
-]]
 
 local mapMenu = nil
-local mai
 
+local statetext = {}
 local maintainancecost = {}
 
 local maintainancedata = {}
@@ -19,8 +16,8 @@ local function Init()
   mapMenu.registerCallback("createPropertyOwned_on_init_infoTableData", maintainancecost.createPropertyOwned_on_init_infoTableData)
   mapMenu.registerCallback("createPropertyOwned_on_add_ship_infoTableData", maintainancecost.createPropertyOwned_on_add_ship_infoTableData)
   mapMenu.registerCallback("createPropertyOwned_on_createPropertySection_unassignedships", maintainancecost.createPropertyOwned_on_createPropertySection_unassignedships)
-  mapMenu.registerCallback("createPropertyRow_on_init_vars", maintainancecost.createPropertyRow_on_init_vars)
   mapMenu.registerCallback("createPropertyRow_override_row_location_createText", maintainancecost.createPropertyRow_override_row_location_createText)
+  mapMenu.registerCallback("MtCst_map_shipInformation_newrowsafterboardingstrength", maintainancecost.setupInfoSubmenuRows_add_maintainancestate)
 end
 
 
@@ -29,12 +26,14 @@ function maintainancecost.Initdata()
   local mdtable = GetNPCBlackboard(playerId, "$maintainancecost_states")
   for ship, data in pairs(mdtable) do
     maintainancedata[ConvertStringTo64Bit(tostring(ship))] = {data[1], data[2]}
-  end 
+  end
+  statetext = {ReadText(90105, 7024), Helper.convertColorToText(mapMenu.holomapcolor.lowalertcolor) .. ReadText(90105, 7025) .. "\027X", Helper.convertColorToText(mapMenu.holomapcolor.mediumalertcolor) .. ReadText(90105, 7026) .. "\027X", Helper.convertColorToText(mapMenu.holomapcolor.highalertcolor) .. ReadText(90105, 7027) .. "\027X"}
 end
 
 
 function maintainancecost.Claerdata()
   maintainancedata = {}
+  statetext = {}
 end
 
 
@@ -79,41 +78,35 @@ function maintainancecost.createPropertyOwned_on_createPropertySection_unassigne
 end
 
 
-function maintainancecost.createPropertyRow_on_init_vars(maxicons, subordinates, dockedships, constructions, convertedComponent, iteration)
-  return nil
-end
-
-
 function maintainancecost.createPropertyRow_override_row_location_createText(locationtext, properties, component)
   local menu = mapMenu
 	local mouseovertext = properties.mouseOverText
   local component64 = ConvertStringTo64Bit(tostring(component))
   if mapMenu.propertyMode == "maintainancecost_info" and maintainancedata[component64] then
-    local state
     local maxhp = tostring(maintainancedata[component64][2])
-    if maintainancedata[component64][1] == 1 then
-      locationtext = ReadText(90105, 7024)
-      state = ReadText(90105, 7024)
-    elseif maintainancedata[component64][1] == 2 then
-      locationtext = Helper.convertColorToText(menu.holomapcolor.lowalertcolor) .. ReadText(90105, 7025)
-      state = ReadText(90105, 7025)
-    elseif maintainancedata[component64][1] == 3 then
-      locationtext = Helper.convertColorToText(menu.holomapcolor.mediumalertcolor) .. ReadText(90105, 7026)
-      state = ReadText(90105, 7026)
-    elseif maintainancedata[component64][1] == 4 then
-      locationtext = Helper.convertColorToText(menu.holomapcolor.highalertcolor) .. ReadText(90105, 7027)
-      state = ReadText(90105, 7027)
+    if statetext[maintainancedata[component64][1]] then
+      locationtext = statetext[maintainancedata[component64][1]]
     else
       locationtext = ReadText(90105, 7028)
-      state = ReadText(90105, 7028)
-      maxhp = ReadText(90105, 7028)
     end
-    mouseovertext = ReadText(90105,7029) .. state ..", " .. ReadText(90105, 7030) .. maxhp .. "%"
+    mouseovertext = ReadText(90105,7029) .. locationtext ..", " .. ReadText(90105, 7030) .. maxhp .. "%"
     if maintainancedata[component64][1] == 4 then
       mouseovertext = mouseovertext .. "\n" .. ReadText(90105, 7031)
     end
   end
   return {locationtext = locationtext, properties = {halign = "center", font = properties.font, mouseOverText = mouseovertext, x = properties.x}}
+end
+
+
+function maintainancecost.setupInfoSubmenuRows_add_maintainancestate(object, object64)
+  local defenceinfo_high = C.IsInfoUnlockedForPlayer(object, "defence_status")
+  local text
+  if (defenceinfo_high and maintainancedata[object64] ~= nil and statetext[maintainancedata[object64][1]]) then
+    text = statetext[maintainancedata[object64][1]]
+  else
+    text = ReadText(90105, 7028)
+  end
+  return { false, ReadText(90105, 7022) .. ReadText(1001, 120), text }	
 end
 
 Init()
